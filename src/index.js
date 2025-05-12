f76dcb87f6936b6b51ba102d106b26092f0e86de
ribondareva <msbondareva01@gmail.com>
2025-05-12 20:18:37 +0300

    Json server added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.getElementById('app').textContent = 'Hello from Webpack!';
 
+fetch('http://localhost:3000/posts')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Данные получены:', data);
+    const container = document.getElementById('data');
+    data.forEach(item => {
+      const div = document.createElement('div');
+      div.textContent = `ID: ${item.id}, Title: ${item.title}, Author: ${item.author}`;
+      container.appendChild(div);
+    });
+  })
+  .catch(error => console.error('Ошибка при получении данных:', error));
 
+fetch('http://localhost:3000/comments')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Данные получены:', data);
+    const container = document.getElementById('data');
+    data.forEach(item => {
+      const div = document.createElement('div');
+      div.textContent = `ID: ${item.id}, Body: ${item.body}, postId: ${item.postId}`;
+      container.appendChild(div);
+    });
+  })
+  .catch(error => console.error('Ошибка при получении данных:', error));
+
+fetch('http://localhost:3000/profile')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Данные получены:', data);
+    const container = document.getElementById('data');
+    data.forEach(item => {
+      const div = document.createElement('div');
+      div.textContent = `Name: ${item.name}`;
+      container.appendChild(div);
+    });
+  })
+  .catch(error => console.error('Ошибка при получении данных:', error));
